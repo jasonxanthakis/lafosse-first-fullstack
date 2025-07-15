6da0903b0ce4c4241b1bdf4993a3c27bbc7a4cfb
jasonxanthakis <iasonas.xanthakis@gmail.com>
2025-07-15 17:42:16 +0100

    Test commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,12 @@
-//const API_KEY = require('./key.js');
-
-//const API_KEY = process.env.API_KEY;
-
-//const API_KEY = window.API_KEY;
-
-const API_KEY = '51162602-6f98114165846c3f1144f02d1';
-
-const fruitList = document.querySelector("#fruitSection ul");
-
-const createForm = document.querySelector("#create-form")
-
 const fruitForm = document.querySelector("#inputSection form");
+const fruitList = document.querySelector("#fruitSection ul");
+const fruitNutrition = document.querySelector("#nutritionSection p");
 
-const style = document.createElement("style");
-style.textContent = "img {width:auto;height:150px}";
-//document.head.appendChild(style);
+let cal = 0;
+const fruitCal = {};
+const apiKey = "API KEY HERE";
 
-fruitForm.addEventListener(
-    "submit",
-    extractFruit
-);
-
-createForm.addEventListener("submit", createNewFruit);
+fruitForm.addEventListener("submit", extractFruit);
 
 function extractFruit(e) {
     e.preventDefault();
@@ -29,88 +14,46 @@ function extractFruit(e) {
     e.target.fruitInput.value = "";
 }
 
-async function createNewFruit(e) {
-    e.preventDefault();
-    const data = {name: e.target.fruitInput.value};
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    }
-    //Make sure to add your deployed API URL in this fetch
-    const response = await fetch(`https://fruit-salad-builder-backend.onrender.com/fruits`, options);
-    let messageStatus = document.querySelector("#message")
-    if(response.status === 201) {
-        e.target.fruitInput.value = ''
-        messageStatus.textContent = "Fruit successfully created."
-        setTimeout(() => {
-          messageStatus.textContent = ""
-        }, 4000)
-    } else {
-        e.target.fruitInput.value = ''
-        messageStatus.textContent = "This fruit already exists. Please enter another fruit!"
-        setTimeout(() => {
-          messageStatus.textContent = ""
-        }, 4000)
+async function fetchFruitData(fruit) {
+    try {
+        //Make sure to replace this link with your deployed API URL in this fetch
+        const respData = await fetch(`https://fruit-api-5v0j.onrender.com/fruits/${fruit}`);
+        const respImg = await fetch(
+            `https://pixabay.com/api/?q=${fruit}+fruit&key=${apiKey}`
+        );
+
+        if (respData.ok && respImg.ok) {
+            const data = await respData.json();
+            const imgData = await respImg.json();
+            addFruit(data, imgData);
+        } else {
+            throw "Something has gone wrong with one of the API requests";
+        }
+    } catch (e) {
+        console.log(e);
     }
 }
 
-let cal = 0;
-function addFruit(fruit) {
-    const li = document.createElement("li");
-    li.textContent = fruit.name;
-    fruitList.appendChild(li);
-    li.addEventListener(
-        "click", 
-        e => deleteFruit(e, fruit.nutritions.calories)
-    );
+function addFruit(fruit, fruitImg) {
+    const img = document.createElement("img");
+    img.classList.add('fruits');
+    img.alt = fruit.name;
+    img.src = fruitImg.hits[0].previewURL;
 
-    img = fetchFruitImage(fruit.name, li, fruit.nutritions.calories);
+    img.addEventListener("click", removeFruit, { once: true });
+    fruitList.appendChild(img);
+
+    fruitCal[fruit.name] = fruit.nutritions.calories;
 
     cal += fruit.nutritions.calories;
-    fruitNutrition.textContent = cal;
+    fruitNutrition.textContent = "Total Calories: " + cal;
 }
 
-function deleteFruit(e, cals) {
+function removeFruit(e) {
+    const fruitName = e.target.alt;
+    cal -= fruitCal[fruitName];
+    fruitNutrition.textContent = "Total Calories: " + cal;
+
+    delete fruitCal[fruitName];
     e.target.remove();
-    //e.target.parentNode.removeChild(e.target)
-
-    cal -= cals;
-    fruitNutrition.textContent = cal;
-}
-
-function deleteFruitImg(e, cals) {
-    // deleteFruit(e.target.parentNode, cals)
-    e.target.parentNode.remove();
-}
-
-function fetchFruitData(fruit) {
-    if (fruit.trim() == "" || ['greenapple'].includes(fruit)) {
-        return;
-    }
-    fetch(`https://fruit-salad-builder-backend.onrender.com/fruits/${fruit}`)
-        .then((resp) => resp.json())
-        .then(data => addFruit(data))
-        .catch((e) => console.log(e));
-}
-
-function fetchFruitImage(fruit, li, cals) {
-    let URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(fruit)+"+fruit&image_type=photo";
-    fetch(URL)
-        .then((resp) => resp.json())
-        .then(data => {
-            let imgURL = data.hits[0].largeImageURL;
-            const img = document.createElement("img");
-            img.setAttribute("src", imgURL);
-            li.appendChild(document.createElement("br"))
-            li.appendChild(img);
-
-            img.addEventListener(
-                "click", 
-                e => deleteFruitImg(e, cals)
-            );
-        })
-        .catch((e) => console.log(e));
 }
